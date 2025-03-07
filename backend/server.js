59cf38d1e6f3bbec6b9829f908411173a96af34c
Kushagra <kseth0808@gmail.com>
2025-03-07 12:52:51 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.use('/data', apps);
 
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
 
 app.listen(port, () => {
     console.log("Server is running on port");
--- frontend/src/appRoutes.js
+++ frontend/src/appRoutes.js
@@ -1,3 +1,3 @@
-const Host = 'https://greetuser.onrender.com/'
+const Host = 'https://greetuser.onrender.com'
 
 export const postGreetUser = `${Host}/data/greetUser`
