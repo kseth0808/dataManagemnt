828a25f6b2f4244bec2d9c7bd53e0ea6a0e869cc
Kushagra <kseth0808@gmail.com>
2025-04-06 20:22:47 +0530

    Remove backend from tracking

--- backend/controller/user/user.js
+++
@@ -1,7 +0,0 @@
-export const greetUser = (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res.status(400).json({ error: "Name is required." });
-    }
-    res.json({ message: `Hello, ${name}! Welcome to Younglabs.` });
-};
--- backend/routes/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { greetUser } from '../controller/user/user.js';
-
-
-const router = express.Router();
-
-router.post("/greetUser", greetUser)
-
-export default router;
